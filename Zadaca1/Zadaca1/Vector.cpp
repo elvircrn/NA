3012de9068d90adc66bf9caba4872d0473b48ef5
elvircn <elvircrn@gmail.com>
2016-10-31 08:14:33 +0100

    Zadaca 1

---
+++ Zadaca1/Zadaca1/Vector.cpp
@@ -0,0 +1,145 @@
+#include "Vector.h"
+
+
+
+void Vector::PushBack(const double & elem)
+{
+	elements.push_back(elem);
+}
+
+void Vector::DimChech(const Vector & v1, const Vector & v2) const
+{
+	if (v1.NElems() != v2.NElems())
+		throw std::domain_error("Incompatible formats");
+}
+
+void Vector::RangeCheck(int index) const
+{
+	if (index < 0 || NElems() <= index)
+		throw std::range_error("Invalid index");
+}
+
+Vector& Vector::ForEach(const std::function<double(double)>& f)
+{
+	for (auto &x : elements)
+		x = f(x);
+	return (*this);
+}
+
+Vector::Vector()
+{
+}
+
+Vector::Vector(int n)
+{
+	elements.resize(n, 0);
+}
+
+Vector::Vector(std::initializer_list<double> l)
+{
+	elements.assign(l.begin(), l.end());
+}
+
+int Vector::NElems() const
+{
+	return elements.size();
+}
+
+double & Vector::operator[](int i)
+{
+	RangeCheck(i);
+	return elements[i];
+}
+
+double Vector::operator[](int i) const
+{
+	RangeCheck(i);
+	return elements[i];
+}
+
+double & Vector::operator()(int i)
+{
+	RangeCheck(i);
+	return elements[i];
+}
+
+double Vector::operator()(int i) const
+{
+	RangeCheck(i);
+	return elements[i];
+}
+
+void Vector::Print(char separator) const
+{
+	bool first(true);
+	for (auto x : elements)
+	{
+		if (!first)
+			putchar(separator);
+		std::cout << x;
+		if (first)
+			first = false;
+	}
+}
+
+Vector & Vector::operator+=(const Vector & v)
+{
+	for (int i = 0; i < v.NElems(); i++)
+		elements[i] += v[i];
+	return (*this);
+}
+
+Vector & Vector::operator-=(const Vector & v)
+{
+	for (int i = 0; i < v.NElems(); i++)
+		elements[i] -= v[i];
+
+	return (*this);
+}
+
+Vector & Vector::operator*=(double s)
+{
+	return ForEach([&s](double x) { return x * s; });
+}
+
+
+Vector::~Vector()
+{
+}
+
+Vector operator+(const Vector & v1, const Vector & v2)
+{
+	Vector ret;
+	for (int i = 0; i < v1.NElems(); i++)
+		ret.PushBack(v1[i] + v2[i]);
+	return ret;
+}
+
+Vector operator-(const Vector & v1, const Vector & v2)
+{
+	Vector ret;
+	for (int i = 0; i < v1.NElems(); i++)
+		ret.PushBack(v1[i] - v2[i]);
+	return ret;
+}
+
+Vector operator*(double s, const Vector &v)
+{
+	Vector ret = v;
+	return ret.ForEach([&s](double x) { return x * s; });
+}
+
+Vector operator*(const Vector & v, double s)
+{
+	Vector ret = v;
+	return ret.ForEach([&s](double x) { return x * s; });
+}
+
+double operator*(const Vector & v1, const Vector & v2)
+{
+	v1.DimChech(v1, v2);
+	double ret = 0.0;
+	for (int i = 0; i < v1.NElems(); i++)
+		ret += v1[i] * v2[i];
+	return ret;
+}
---
+++ Zadaca1/Zadaca1/Vector.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <functional>
+
+class Vector
+{
+	std::vector<double> elements;
+
+	void PushBack(const double &elem);
+	void DimChech(const Vector &v1, const Vector &v2) const;
+	void RangeCheck(int index) const;
+	Vector& ForEach(const std::function<double(double)> &f);
+public:
+	Vector();
+	
+	explicit Vector(int n);
+	Vector(std::initializer_list<double> l);
+	int NElems() const;
+	double &operator[](int i);
+	double operator[](int i) const;
+	double &operator()(int i);
+	double operator()(int i) const;
+	void Print(char separator = '\n') const;
+	friend Vector operator +(const Vector &v1, const Vector &v2);
+	Vector &operator +=(const Vector &v);
+	friend Vector operator -(const Vector &v1, const Vector &v2);
+	Vector &operator -=(const Vector &v);
+	friend Vector operator *(double s, const Vector &v);
+	friend Vector operator *(const Vector &v, double s);
+	Vector &operator *=(double s);
+	friend double operator *(const Vector &v1, const Vector &v2);
+	friend Vector operator /(const Vector &v, double s);
+	Vector &operator /=(double s);
+	
+	~Vector();
+};
+
